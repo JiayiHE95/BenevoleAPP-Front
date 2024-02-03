8870ca7ec1ebb7e7d645f475122e8724470f1a7d
Charlène <charlene.morchipont@etu.umontpellier.fr>
2024-02-03 18:20:17 +0100

    css signup

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -83,130 +83,141 @@ const addUser=()=>{
 }
 
 console.log(taille_tshirt)
+return (
+  <div className="signup-containers">
+    <h1>Inscription</h1>
+    <div className="signup-form-containers">
+      <label htmlFor="pseudo" className="signup-labels">Pseudo:</label>
+      <input
+        type="text"
+        id="pseudo"
+        name="pseudo"
+        className="signup-input-fields"
+        onChange={(e) => { setPseudo(e.target.value) }}
+        required
+      />
 
- return(
-  <div>
-      <h1>Signup</h1>
-      <div>
-        <label htmlFor="pseudo">Pseudo:</label>
-        <input
-          type="text"
-          id="pseudo"
-          name="pseudo"
-          onChange={(e)=>{setPseudo(e.target.value)}} 
-          required
-        />
+      <label htmlFor="nom" className="signup-labels">Nom:</label>
+      <input
+        type="text"
+        id="nom"
+        name="nom"
+        className="signup-input-fields"
+        onChange={(e) => { setNom(e.target.value) }}
+        required
+      />
 
-        <label htmlFor="nom">Nom:</label>
-        <input
-          type="text"
-          id="nom"
-          name="nom"
-          onChange={(e)=>{setNom(e.target.value)}} 
-          required
-        />
+      <label htmlFor="prenom" className="signup-labels">Prénom:</label>
+      <input
+        type="text"
+        id="prenom"
+        name="prenom"
+        className="signup-input-fields"
+        onChange={(e) => { setPrenom(e.target.value) }}
+        required
+      />
 
-        <label htmlFor="prenom">Prénom:</label>
-        <input
-          type="text"
-          id="prenom"
-          name="prenom"
-          onChange={(e)=>{setPrenom(e.target.value)}}
-          required
-        />
+      <label htmlFor="tel" className="signup-labels">Téléphone:</label>
+      <input
+        type="tel"
+        id="tel"
+        name="tel"
+        className="signup-input-fields"
+        onChange={(e) => { setTel(e.target.value) }}
+        required
+      />
 
-        <label htmlFor="tel">Téléphone:</label>
-        <input
-          type="tel"
-          id="tel"
-          name="tel"
-          onChange={(e)=>{setTel(e.target.value)}}
-          required
-        />
+      <label htmlFor="mail" className="signup-labels">Email:</label>
+      <input
+        type="email"
+        id="mail"
+        name="mail"
+        className="signup-input-fields"
+        onChange={(e) => { setMail(e.target.value) }}
+        required
+      />
 
-        <label htmlFor="mail">Email:</label>
-        <input
-          type="email"
-          id="mail"
-          name="mail"
-          onChange={(e)=>{setMail(e.target.value)}}
-          required
-        />
+      <label htmlFor="association" className="signup-labels">Association:</label>
+      <input
+        type="text"
+        id="association"
+        name="association"
+        className="signup-input-fields"
+        onChange={(e) => { setAssociation(e.target.value) }}
+      />
 
-        <label htmlFor="association">Association:</label>
-        <input
-          type="text"
-          id="association"
-          name="association"
-          onChange={(e)=>{setAssociation(e.target.value)}}
-        />
+      <label htmlFor="taille_tshirt" className="signup-labels">Taille Tshirt:</label>
+      <select
+        id="taille_tshirt"
+        name="taille_tshirt"
+        className="signup-input-fields"
+        onChange={(e) => { setTailleT(e.target.value) }}
+        required
+      >
+        <option value="XS">XS</option>
+        <option value="S">S</option>
+        <option value="M">M</option>
+        <option value="L">L</option>
+        <option value="XL">XL</option>
+        <option value="XXL">XXL</option>
+      </select>
 
-        <label htmlFor="taille_tshirt">Taille Tshirt:</label>
-        <select
-          id="taille_tshirt"
-          name="taille_tshirt"
-          onChange={(e)=>{setTailleT(e.target.value)}}
-          required
-        >
-          <option value="XS">XS</option>
-          <option value="S">S</option>
-          <option value="M">M</option>
-          <option value="L">L</option>
-          <option value="XL">XL</option>
-          <option value="XXL">XXL</option>
-        </select>
+      <label htmlFor="est_vegetarien" className="signup-labels">Est Végétarien:</label>
+      <input
+        type="checkbox"
+        id="est_vegetarien"
+        name="est_vegetarien"
+        className="signup-checkbox-fields"
+        onChange={(e) => { setEst_vegetarien(e.target.value) }}
+      />
 
-        <label htmlFor="est_vegetarien">Est Végétarien:</label>
-        <input
-          type="checkbox"
-          id="est_vegetarien"
-          name="est_vegetarien"
-          onChange={(e)=>{setEst_vegetarien(e.target.value)}}
-        />
+      <label htmlFor="hebergement" className="signup-labels">Hébergement:</label>
+      <input
+        type="text"
+        id="hebergement"
+        name="hebergement"
+        className="signup-input-fields"
+        onChange={(e) => { setHebergement(e.target.value) }}
+      />
 
-        <label htmlFor="hebergement">Hébergement:</label>
-        <input
-          type="text"
-          id="hebergement"
-          name="hebergement"
-          onChange={(e)=>{setHebergement(e.target.value)}}
-        />
+      <label htmlFor="jeu_prefere" className="signup-labels">Jeu Préféré:</label>
+      <input
+        type="text"
+        id="jeu_prefere"
+        name="jeu_prefere"
+        className="signup-input-fields"
+        onChange={(e) => { setJeu_prefere(e.target.value) }}
+      />
 
-        <label htmlFor="jeu_prefere">Jeu Préféré:</label>
-        <input
-          type="text"
-          id="jeu_prefere"
-          name="jeu_prefere"
-          onChange={(e)=>{setJeu_prefere(e.target.value)}}
-        />
+      <label htmlFor="mdp" className="signup-labels">Mot de Passe:</label>
+      <input
+        type="password"
+        id="mdp"
+        name="mdp"
+        className="signup-input-fields"
+        onChange={(e) => { setMdp(e.target.value) }}
+        required
+      />
 
-        <label htmlFor="mdp">Mot de Passe:</label>
-        <input
-          type="password"
-          id="mdp"
-          name="mdp"
-          onChange={(e)=>{setMdp(e.target.value)}}
-          required
-        />
-
-        <label htmlFor="mdp-reset">Resaisir le mot de Passe:</label>
-        <input
-          type="password"
-          id="mdp-reset"
-          name="mdp"
-          onChange={(e)=>{setResetPassword(e.target.value)}}
-          required
-        />
-        {mailNotif&& <div className='notif-error'><TbAlertCircle className='error-icon'/><div>Mail déjà utilisé</div></div>}
-        {passwordNotif&& <div className='notif-error'><TbAlertCircle className='error-icon'/><div>Mots de passe non identiques</div></div>}
-        {notif&& <div className='notif-error'><TbAlertCircle className='error-icon'/><div>Aucun champs ne peut être vide</div></div>}
-        {connexionNotif&& <div className='notif-error'><TbAlertCircle className='error-icon'/><div>Connexion Automatique échouée, veuillez vous connecter ultérieurement</div></div>}
-        {createUserNotif&& <div className='notif-error'><TbAlertCircle className='error-icon'/><div>Création de compte échouée, veuillez essayer ultérieurement</div></div>}
-        <div onClick={()=>{navigate("/login")}}>J'ai déjà un compte</div>
-        <button type="submit" onClick={()=>addUser()}>Sign Up</button>
-      </div>
+      <label htmlFor="mdp-reset" className="signup-labels">Resaisir le mot de Passe:</label>
+      <input
+        type="password"
+        id="mdp-reset"
+        name="mdp"
+        className="signup-input-fields"
+        onChange={(e) => { setResetPassword(e.target.value) }}
+        required
+      />
+      {mailNotif && <div className='signup-notif-errors'><TbAlertCircle className='signup-error-icons' /><div>Mail déjà utilisé</div></div>}
+      {passwordNotif && <div className='signup-notif-errors'><TbAlertCircle className='signup-error-icons' /><div>Mots de passe non identiques</div></div>}
+      {notif && <div className='signup-notif-errors'><TbAlertCircle className='signup-error-icons' /><div>Aucun champ ne peut être vide</div></div>}
+      <div className='signup-clickables' onClick={() => { navigate("/login") }}>J'ai déjà un compte</div>
+      <button type="submit" className='signup-submit-buttons' onClick={() => addUser()}>Sign Up</button>
     </div>
- )
+  </div>
+);
+
+
 
 }
 export default SignUp
