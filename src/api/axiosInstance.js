a528390b248b4a35e4ed15a2eaae855311fe45dd
Charlène <charlene.morchipont@etu.umontpellier.fr>
2024-02-04 13:27:23 +0100

    ajout middleware back

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-
+axios.defaults.headers.common['Authorization']= 'Bearer '+ localStorage.getItem('accessToken');
 const axiosInstance = axios.create({
     baseURL: 'http://localhost:3005', // URL de base de l'API
   });
